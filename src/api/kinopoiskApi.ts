c9f2295e5817889f6cb4d3903493301587b8f96c
project Amir <amirzhanchikk@gmail.com>
2025-08-12 12:17:54 +0500

    new function api

--- src/api/kinopoiskApi.ts
+++ src/api/kinopoiskApi.ts
@@ -1,12 +1,24 @@
 import axios from "axios";
 
-const API_KEY = "EPJEEZD-RPT42ZD-GS9F7VW-E5NV13Z";
+const API_KEY = "H3HV3S7-BY0M33Z-KEASEEK-FH7V4E8";
 const API_URL = "https://api.kinopoisk.dev/v1.4";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: { "X-API-KEY": API_KEY },
 });
+
+export async function imageCenter(limit = 5) {
+  const res = await api.get("/movie", {
+    params: {
+      "rating.kp": "8-9",
+      limit,
+      sortField: "year",
+      sortType: "-1",
+    },
+  });
+  return res.data.docs;
+}
 
 export async function getRecommended(limit = 4) {
   const res = await api.get("/movie", {
@@ -32,7 +44,36 @@ export async function getTrending(limit = 4) {
   return res.data.docs;
 }
 
+export async function getNewRelease(limit = 4) {
+  const res = await api.get("/movie", {
+    params: {
+      limit,
+      "year": "2025-2025",
+      sortField: "votes.kp",
+      sortType: "-1",
+    },
+  });
+  return res.data.docs;
+}
+
+export async function getRecommendedMovies(limit = 4) {
+  const res = await api.get("/movie", {
+    params: {
+      limit,
+      "year": "1999-2020",
+      sortField: "votes.kp",
+      sortType: "-1",
+    },
+  });
+  return res.data.docs;
+}
+
+
+
 export default {
   getRecommended,
   getTrending,
+  getNewRelease,
+  getRecommendedMovies,
+  imageCenter,
 };
